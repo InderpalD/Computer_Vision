4b60621afcc3d8d307f2af30ffc360517f8b814b
Paul Dhillon <inderpald3@gmail.com>
2021-02-24 21:28:36 -0500

    Update live_video.py

--- live_video.py
+++ live_video.py
@@ -1,27 +1,26 @@
 import cv2
 import time
 
-
-class live_video(object):
+class Live_Video(object):
+	''' Uses webcam to start a live video '''
 	
 	def __init__(self):
+		'''constructor'''
 		pass
 
 	def record(self):
-		
+		''' Start recording using your camera feed '''
 		video = cv2.VideoCapture(0)
-
 		while(True):
 
 			# get the frame (numpy array)
 			ret, frame = video.read()
-
 			cv2.imshow("pic", frame)
 	
 			if not ret:
 				break
 	
-			# this will ensure we create a new frame after 1 millisecond 
+			# this will ensure we create a new frame
 			key = cv2.waitKey(1)
 	
 			if key == ord('q'):
@@ -33,5 +32,5 @@ class live_video(object):
 if __name__ == '__main__':
 
 	# Press q on the window to quit recording
-	vid = live_video()		
+	vid = Live_Video()		
 	vid.record()
